e6eb05eb08faa3a26540bc60da0589abfd01adbd
UWERA Divine <divinebeulla716@gmail.com>
2023-06-22 22:55:22 +0200

    added speaker CRUD operation

--- speakers/urls.py
+++ speakers/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from speakers import views
 urlpatterns = [
-    path('', views.speaker_list, name='conference'),
-    path('create/', views.create_speaker, name='create_a_new_speaker'),
-    path('<int:speaker_id>/', views.read_speaker, name='view_single_speaker'),
-    path('<int:speaker_id>/update', views.update_speaker, name='update_speaker'),
-    path('<int:speaker_id>/delete', views.delete_speaker, name='delete_speaker'),
+    path('', views.speaker_list),
+    path('create/', views.create_speaker),
+    path('<int:speaker_id>/', views.read_speaker),
+    path('<int:speaker_id>/update', views.update_speaker),
+    path('<int:speaker_id>/delete', views.delete_speaker),
 ]
 
